c0eed2238c95b1cb0b17396551cfdaea4293ad4b
ajokea <ajokenakinseye@gmail.com>
2025-06-18 20:37:01 -0400

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+});
